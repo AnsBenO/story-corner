f80e768f6e136d334b29515d7d33256179743dc5
AnsBenO <ans.benomar@gmail.com>
2024-09-05 23:13:20 +0100

    trigger build

--- books-service/src/test/java/com/ansbeno/books_service/api/controllers/NotificationControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.ansbeno.books_service.api.controllers;
-
-import com.ansbeno.books_service.AbstractIntegrationTest;
-
-public class NotificationControllerTest extends AbstractIntegrationTest {
-
-}
